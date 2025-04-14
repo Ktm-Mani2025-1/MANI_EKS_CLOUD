eb7e3e6968ff1bb123aadb8a8a5b3ec0568c4872
Ktm-Mani2025-1 <ktmsells@gmail.com>
2025-04-14 18:25:02 +0545

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mani-Cloud1" # Replace with your actual S3 bucket name
+    bucket = "mani-cloud1" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "eu-north-1"
   }
